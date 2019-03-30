b0887fa899205262da5bdc28bc483ce52ae5bcc6
Wayne Maurer <wayne@waynemaurer.net>
2019-03-30 22:10:40 +0100

    from james

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState, useCallback } from 'react';
 
-class App extends Component {
-  render() {
+type ItemData = { id: number; name: string };
+
+const itemData: ItemData[] = [
+    { id: 1, name: 'Milk Chocolate' },
+    { id: 2, name: 'Dark Chocolate' },
+    { id: 3, name: 'White Chocolate' },
+    { id: 4, name: 'Raw Chocolate' },
+    { id: 5, name: 'Chocolate Milk' },
+];
+
+let lastColor: string;
+
+function generateNewColor() {
+    lastColor = 'rgba(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ',1)';
+}
+
+const Row = React.memo<{ item: ItemData; editing: boolean; onEdit: (id: number) => void }>(
+    ({ item, editing, onEdit }) => {
+        return (
+            <div
+                style={{
+                    borderBottom: '1px solid #f0f0f0',
+                    padding: 10,
+                    display: 'flex',
+                }}
+                onClick={() => onEdit(item.id)}
+            >
+                <div style={{ flex: 1 }}>{editing ? <input defaultValue={item.name} /> : item.name}</div>
+                <div style={{ backgroundColor: lastColor, width: 25, height: 25 }} />
+            </div>
+        );
+    },
+);
+
+function App() {
+    let [items] = useState(itemData);
+    let [editingId, setEditingId] = useState<number | null>(null);
+
+    generateNewColor();
+
+    function onEdit(id: number) {
+        setEditingId(id);
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            {items.map(item => (
+                <Row key={item.id} item={item} editing={editingId === item.id} onEdit={onEdit} />
+            ))}
+        </div>
     );
-  }
 }
 
 export default App;
